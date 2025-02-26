80c88bacef9528a207a48e1205f0bb5a6540a344
Dave Braid <d.aragones91@gmail.com>
2025-02-26 16:07:25 +0100

    26_02 Session
    Character controller finished

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -15,24 +14,40 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpForce;
     [SerializeField] private float gravity = 9.81f;
     [SerializeField] private float stickToGroundVelocity;
+    [Header("Slide Stuff")]
+    [SerializeField] private float slideSpeed;
+    [SerializeField] private float slideSlope = 45;
+    [SerializeField] private float slideSlowdownTime = 2;
+    [SerializeField] private float slideRampUpFactor = 3;
+    [SerializeField] private float slideRampDownFactor = 5;
+    [SerializeField] private float slideFactorRampUp = 10;
+    [SerializeField] private AnimationCurve slideSlowDownCurve = AnimationCurve.EaseInOut(0f, 1f, 1f, 0f);
 
     private CharacterController _characterController;
 
     private Vector3 _playerVelocity;
     private float _verticalVelocity;
+    private Vector3 _slideVelocity;
+    private float _slideVelocityFactor = 1;
+    private float _slidingTime;
+    private float _slidingSlowdownTimeInverse;
     
     private bool _isJumping = false;
     private bool _jumpEnded = true;
+    private bool _isSliding = true;
     
     private void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        slideSlope = _characterController.slopeLimit;
+        _slidingSlowdownTimeInverse = 1 / slideSlowdownTime;
     }
 
     private void Update()
     {
         UpdateMoveVelocity();
         UpdateVerticalVelocity();
+        UpdateSlideVelocity();
 
         ApplyTotalVelocity();
 
@@ -41,7 +56,7 @@ public class PlayerController : MonoBehaviour
 
     private void UpdateVerticalVelocity()
     {
-        if (Input.GetAxisRaw("Jump") > 0.5f && _characterController.isGrounded && !_isJumping)
+        if (Input.GetAxisRaw("Jump") > 0.5f && _characterController.isGrounded && !_isJumping && !_isSliding)
         {
             _isJumping = true;
            _jumpEnded = false;
@@ -74,15 +89,17 @@ public class PlayerController : MonoBehaviour
 
     private void ApplyTotalVelocity()
     {
-        var totalVelocity = _playerVelocity + _verticalVelocity * Vector3.up;
+        var totalVelocity = _playerVelocity + _verticalVelocity * Vector3.up + _slideVelocity * _slideVelocityFactor;
         
         _characterController.Move(totalVelocity * Time.deltaTime);
     }
 
     private void UpdateMoveVelocity()
     {
-        var xInput = Input.GetAxis("Horizontal");
-        var yInput = Input.GetAxis("Vertical");
+        
+        
+        var xInput = /*!_isSliding ?*/ Input.GetAxis("Horizontal") /*: 0*/;
+        var yInput = !_isSliding ? Input.GetAxis("Vertical") : 0;
         
         var input = cam.transform.right * xInput + cam.transform.forward * yInput;
         input.y = 0;
@@ -92,5 +109,44 @@ public class PlayerController : MonoBehaviour
         input = new Vector3(input.x * sideSpeed, 0, input.z * forwardSpeed);
         
         _playerVelocity = input;
+    }
+
+    private void UpdateSlideVelocity()
+    {
+        var maxSlideVelocity = Vector3.zero;
+        RaycastHit hit;
+        if (_characterController.isGrounded && Physics.SphereCast(transform.position + _characterController.center,
+                _characterController.radius, Vector3.down, out hit))
+        {
+            var angle = Vector3.Angle(hit.normal, Vector3.up);
+            print(angle);
+
+            if (angle > slideSlope)
+            {
+                _isSliding = true;
+                
+                var slideDirection = Vector3.ProjectOnPlane(hit.normal, Vector3.down).normalized;
+                maxSlideVelocity = slideDirection * slideSpeed;
+                
+                Debug.DrawRay(hit.point, hit.normal, Color.red, 3f);
+                Debug.DrawRay(hit.point, slideDirection, Color.blue, 3f);
+            }
+            else
+            {
+                _isSliding = false;
+                _slidingTime = 0;
+            }
+            
+            if(_isSliding)
+                _slidingTime += Time.deltaTime;
+            
+            _slideVelocity = _isSliding
+                ? Vector3.Lerp(_slideVelocity, maxSlideVelocity, Time.deltaTime * slideRampUpFactor)
+                : Vector3.Lerp(_slideVelocity, Vector3.zero, Time.deltaTime * slideRampDownFactor);
+            
+            _slideVelocityFactor = _isSliding
+                ? slideSlowDownCurve.Evaluate(Mathf.Clamp01(_slidingTime * _slidingSlowdownTimeInverse))
+                : Mathf.Lerp(_slideVelocityFactor, 1, Time.deltaTime * slideFactorRampUp);
+        }
     }
 }
